4f7f249bcbcd6debcb96d72d6bafa0a652e28950
Ghidyon <gideonakunana@gmail.com>
2021-10-13 16:29:56 +0300

    added models

---
+++ LibraryApi.Models/Entities/Book.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryApi.Models.Entities
+{
+    public class Book
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string ISBN { get; set; }
+        public DateTime YearPublished { get; set; }
+        public Guid AuthorId { get; set; }
+        public virtual Author Author { get; set; }
+        public ICollection<Category> Categories { get; set; }
+    }
+}
---
+++ LibraryApi.Models/Entities/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using LibraryApi.Models.Enumerators;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryApi.Models.Entities
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public Gender Gender { get; set; }
+    }
+}
