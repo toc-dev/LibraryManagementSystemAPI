7d3e2d170851d640d2443dfce2d99cf34dd6d1ab
Ghidyon <gideonakunana@gmail.com>
2021-10-13 23:00:42 +0300

    added activity model

---
+++ LibraryApi.Models/Entities/Activity.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryApi.Models.Entities
+{
+    public class Activity
+    {
+        public Guid Id { get; set; }
+        public Guid UserId { get; set; }
+        public Guid BookId { get; set; }
+        public Book Book { get; set; }
+        public DateTime RequestDate { get; set; }
+        public DateTime DueDate { get; set; }
+    }
+}
