c4a2d5fabc3cb9ec439f4b64ce691dd08636958f
KCMicheal <kcmicheal@users.noreply.github.com>
2021-10-17 19:29:41 +0100

    created repository

---
+++ LibraryApi.Data/Implementations/Repository.cs
@@ -0,0 +1,84 @@
+﻿using LibraryApi.Data.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryApi.Data.Implementations
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private bool disposedValue = false;
+        private readonly DbContext _dbContext;
+        private readonly DbSet<T> _dbSet;
+
+        public Repository(DbContext context)
+        {
+            _dbContext = context ?? throw new ArgumentException(nameof(context));
+            _dbSet = _dbContext.Set<T>();
+        }
+
+        public T Add(T obj)
+        {
+            _dbContext.Add(obj);
+            return obj;
+        }
+
+        public async Task<T> AddAsync(T obj)
+        {
+            Add(obj);
+            await _dbContext.SaveChangesAsync();
+            return obj;
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            return _dbSet.ToList();
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync()
+        {
+            return await _dbSet.ToListAsync();
+
+        }
+
+        public IEnumerable<T> GetByCondition(Expression<Func<T, bool>> predicate = null, Func<IQueryable, IOrderedQueryable> orderby = null, int? skip = null, int? take = null, params string[] includePropertiess)
+        {
+            if (predicate is null) return _dbSet.ToList();
+            return _dbSet.Where(predicate);
+        }
+
+        public T GetById(object id)
+        {
+            return _dbSet.Find(id);
+        }
+
+        public async Task<T> GetByIdAsync(object id)
+        {
+            return await _dbSet.FindAsync(id);
+        }
+
+        public T GetSingleByCondition(Expression<Func<T, bool>> predicate = null, Func<IQueryable, IOrderedQueryable> orderby = null, params string[] includeProperties)
+        {
+            if (predicate is null) return _dbSet.ToList().FirstOrDefault();
+            return _dbSet.Where(predicate).FirstOrDefault();
+        }
+
+        public async Task<bool> AnyAsync(Expression<Func<T, bool>> predicate = null)
+        {
+            if (predicate is null) return await _dbSet.AnyAsync();
+            return await _dbSet.AnyAsync(predicate);
+        }
+
+        public bool Any(Expression<Func<T, bool>> predicate = null)
+        {
+            if (predicate is null) return _dbSet.Any();
+            return _dbSet.Any(predicate);
+        }
+
+       
+    }
+}
---
+++ LibraryApi.Data/Interfaces/IRepository.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryApi.Data.Interfaces
+{
+    public interface IRepository<T>
+    {
+        T Add(T obj);
+        Task<T> AddAsync(T obj);
+
+        IEnumerable<T> GetAll();
+        Task<IEnumerable<T>> GetAllAsync();
+
+        T GetById(object id);
+        Task<T> GetByIdAsync(object id);
+
+        T GetSingleByCondition(Expression<Func<T, bool>> predicate = null,
+            Func<IQueryable, IOrderedQueryable> orderby = null,
+            params string[] includeProperties); 
+        IEnumerable<T> GetByCondition(Expression<Func<T, bool>> predicate = null,
+            Func<IQueryable, IOrderedQueryable> orderby = null,
+            int? skip = null, int? take = null,
+            params string[] includeProperties);
+
+        bool Any(Expression<Func<T, bool>> predicate = null);
+        Task<bool> AnyAsync(Expression<Func<T, bool>> predicate = null);
+        
+
+    }
+}
