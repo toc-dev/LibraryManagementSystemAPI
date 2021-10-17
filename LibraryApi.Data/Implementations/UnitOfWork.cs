f70af02f53300d56c4a85bcc1157e8a597fb83be
Ghidyon <gideonakunana@gmail.com>
2021-10-17 21:32:42 +0300

    perf: added unit of work

---
+++ LibraryApi.Data/Implementations/UnitOfWork.cs
@@ -0,0 +1,44 @@
+﻿using LibraryApi.Data.Interfaces;
+using LibraryApi.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryApi.Data.Implementations
+{
+    public class UnitOfWork<TContext> : IUnitOfWork<IdentityContext> where TContext : IdentityContext
+    {
+        private Dictionary<Type, object> _repositories;
+        private readonly TContext _context;
+
+        public UnitOfWork(TContext context)
+        {
+            _context = context;
+        }
+
+        public IRepository<TEntity> GetRepository<TEntity>() where TEntity : class
+        {
+            if (_repositories is null)
+                _repositories = new Dictionary<Type, object>();
+
+            var type = typeof(TEntity);
+
+            if (!_repositories.ContainsKey(type))
+                _repositories[type] = new Repository<TEntity>(_context);
+
+            return (IRepository<TEntity>)_repositories[type];
+        }
+
+        public int SaveChanges()
+        {
+            return _context.SaveChanges();
+        }
+
+        public async Task<int> SaveChangesAsync()
+        {
+            return await _context.SaveChangesAsync();
+        }
+    }
+}
---
+++ LibraryApi.Data/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryApi.Data.Interfaces
+{
+    public interface IUnitOfWork
+    {
+        IRepository<TEntity> GetRepository<TEntity>() where TEntity : class;
+
+        int SaveChanges();
+
+        Task<int> SaveChangesAsync();
+    }
+
+    public interface IUnitOfWork<TContext> : IUnitOfWork
+    {
+    }
+}
