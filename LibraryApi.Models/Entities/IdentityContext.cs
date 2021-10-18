72c88a1b6a5290b72477432b0d3ee91f4464f535
Ghidyon <gideonakunana@gmail.com>
2021-10-18 13:18:23 +0300

    perf: added user services

--- LibraryApi.Models/Entities/IdentityContext.cs
+++ LibraryApi.Models/Entities/IdentityContext.cs
@@ -40,8 +40,6 @@ namespace LibraryApi.Models.Entities
             builder.ApplyConfiguration<Book>(new BookConfiguration());
         }
 
-        public DbSet<User> Users { get; set; }
-        public DbSet<Role> Roles { get; set; }
         public DbSet<Author> Authors { get; set; }
         public DbSet<Book> Books { get; set; }
         public DbSet<Category> Categories { get; set; }
