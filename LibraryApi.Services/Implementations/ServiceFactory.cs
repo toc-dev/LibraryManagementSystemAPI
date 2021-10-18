876836a70875deeab5b16e2f1d9713da865c047f
Ghidyon <gideonakunana@gmail.com>
2021-10-18 04:06:02 +0300

    perf: added service factory

---
+++ LibraryApi.Services/Implementations/ServiceFactory.cs
@@ -0,0 +1,25 @@
+﻿using LibraryApi.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryApi.Services.Implementations
+{
+    public class ServiceFactory : IServiceFactory
+    {
+        private readonly IServiceProvider _serviceProvider;
+
+        public ServiceFactory(IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+        }
+
+        public T GetService<T>() where T : class
+        {
+            var service = _serviceProvider.GetService(typeof(T));
+            return (T)service;
+        }
+    }
+}
