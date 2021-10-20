2f5f9610b8d1e6052fad43024edc2a17e958ebab
KCMicheal <kcmicheal@users.noreply.github.com>
2021-10-20 11:17:37 +0100

    perf: authorcontroller and activityservvice

---
+++ LibraryApi.Services/Implementations/ActivityService.cs
@@ -0,0 +1,35 @@
+﻿using LibraryApi.Data.Interfaces;
+using LibraryApi.Models.Entities;
+using LibraryApi.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryApi.Services.Implementations
+{
+    public class ActivityService : IActivityService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IRepository<Activity> _activityRepo;
+        private readonly IRepository<User> _userRepo;
+
+        public ActivityService(IUnitOfWork unitOfWork, IUserService userService)
+        {
+            _unitOfWork = unitOfWork;
+            _activityRepo = unitOfWork.GetRepository<Activity>();
+            _userRepo = unitOfWork.GetRepository<User>();
+
+        }
+        public async Task<IEnumerable<Activity>> GetActivities()
+        {
+           return await _activityRepo.GetAllAsync();
+        }
+
+        public async Task<IEnumerable<Activity>> GetUserActivities(Guid id)
+        {
+            return await Task.FromResult(_activityRepo.GetByCondition(b => b.UserId == id));
+        }
+    }
+}
---
+++ LibraryApi.Services/Interfaces/IActivityService.cs
@@ -0,0 +1,15 @@
+﻿using LibraryApi.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryApi.Services.Interfaces
+{
+    public interface IActivityService
+    {
+        Task<IEnumerable<Activity>> GetActivities();
+        Task<IEnumerable<Activity>> GetUserActivities(Guid id);
+    }
+}
