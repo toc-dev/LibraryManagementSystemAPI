b80fc744c1b498c93df0b44ad409011be89d4b53
KCMicheal <kcmicheal@users.noreply.github.com>
2021-10-21 01:42:01 +0100

    perf: added more methods to the author controller

---
+++ LibraryApi.Models/Dtos/ViewBookDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryApi.Models.Dtos
+{
+    public class ViewBookDto
+    {
+        public string Title { get; set; }
+        public Guid AuthorId { get; set; }
+        public DateTime YearPublished { get; set; }
+    }
+}
