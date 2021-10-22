2216cb87d8028cc5e2f74180e1abbeabfb52f00b
Ghidyon <gideonakunana@gmail.com>
2021-10-22 22:20:11 +0300

    perf: added policy claims to books and categories controllers

--- LibraryApi.Services/Interfaces/IUserService.cs
+++ LibraryApi.Services/Interfaces/IUserService.cs
@@ -10,31 +10,6 @@ namespace LibraryApi.Services.Interfaces
 {
     public interface IUserService
     {
-        Task<IEnumerable<ViewBookDto>> GetBooksAsync();
-
-        Task<ViewBookDto> GetBookByIdAsync(Guid id);
-
-        Task<IEnumerable<ViewBookDto>> GetBooksByCategoryAsync(string category);
-
-        Task<Author> CreateAuthorAsync(Author author);
-
-        void UpdateAuthorAsync(Author author);
-
-        void DeleteAuthor(Author author);
-
-        Task<Category> CreateCategoryAsync(Category category);
-
-        Task<IEnumerable<Category>> GetCategoriesAsync();
-
-        Task<Category> GetCategoryAsync(Guid id);
-
-        void UpdateCategory(Category category);
-
-        void DeleteCategory(Category category);
-
-        Task<IEnumerable<Activity>> GetActivitiesAsync();
-
-        Task<IEnumerable<Activity>> GetUserActivitiesAsync(Guid userId);
 
     }
 }
