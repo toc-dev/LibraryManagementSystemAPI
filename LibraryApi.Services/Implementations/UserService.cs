fcd216ec4321c72daf59ddad545f507c5f34a522
Tochukwu <tochukwusage4@gmail.com>
2021-11-01 11:36:01 +0100

    fat controller reduced

--- LibraryApi.Services/Implementations/UserService.cs
+++ LibraryApi.Services/Implementations/UserService.cs
@@ -1,14 +1,18 @@
-﻿using LibraryApi.Data.Interfaces;
+﻿using AutoMapper;
+using LibraryApi.Data.Interfaces;
 using LibraryApi.Models.Dtos;
 using LibraryApi.Models.Entities;
-/**
+
 using LibraryApi.Services.Interfaces;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.JsonPatch;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Mvc;
 
 namespace LibraryApi.Services.Implementations
 {
@@ -17,21 +21,37 @@ namespace LibraryApi.Services.Implementations
         private readonly IUnitOfWork _unitOfWork;
         private readonly IRepository<User> _userRepository;
         private readonly UserManager<User> _userManager;
+        private readonly IMapper _mapper;
 
         public UserService(IUnitOfWork unitOfWork,
             IRepository<User> userRepository,
-            UserManager<User> userManager
+            UserManager<User> userManager,
+            IMapper mapper
             )
         {
             _unitOfWork = unitOfWork;
             _userRepository = userRepository;
             _userManager = userManager;
+            _mapper = mapper;
         }
 
-        public async Task<User> CreateUserAsync(User user)
+        public async Task<(IdentityResult, User)> CreateUserAsync([FromBody] UserForRegistrationDTO userForRegistration)
         {
+            var user = _mapper.Map<User>(userForRegistration);
+            var result = await _userManager.CreateAsync(user, userForRegistration.Password);
 
+            return (result, user);
+
+        }
+
+        public async Task<User> PatchUser(string id, [FromBody] JsonPatchDocument<UserForUpdateDTO> userForUpdate)
+        {
+            var user = await _userManager.FindByIdAsync(id);
+            var userToUpdate = _mapper.Map<UserForUpdateDTO>(user);
+            userForUpdate.ApplyTo(userToUpdate);
+            _mapper.Map(userToUpdate, user);
+
+            return user;
         }
     }
 }
-**/
--- LibraryApi/Controllers/AccountController.cs
+++ LibraryApi/Controllers/AccountController.cs
@@ -15,6 +15,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
+using LibraryApi.Services.Implementations;
 
 namespace LibraryApi.Controllers
 {
@@ -26,11 +27,13 @@ namespace LibraryApi.Controllers
         private readonly UserManager<User> _userManager;
         private readonly IMapper _mapper;
         private readonly IAuthenticationManager _authenticationManager;
+        private readonly UserService _userService;
         
 
         public AccountController(IdentityContext context,
             IMapper mapper,
             UserManager<User> userManager,
+            UserService userService,
             IAuthenticationManager authenticationManager)
             
         {
@@ -38,6 +41,7 @@ namespace LibraryApi.Controllers
             _mapper = mapper;
             _userManager = userManager;
             _authenticationManager = authenticationManager;
+            _userService = userService;
             
         }
 
@@ -54,21 +58,16 @@ namespace LibraryApi.Controllers
         public async Task<IActionResult> RegisterUser([FromBody] UserForRegistrationDTO userForRegistration)
         {
             if (userForRegistration == null) return BadRequest("Invalid!!");
-            var user = _mapper.Map<User>(userForRegistration);
-            var result = await _userManager.CreateAsync(user, userForRegistration.Password);
+            var result = await _userService.CreateUserAsync(userForRegistration);
 
-
-            if (!result.Succeeded)
+            if (!result.Item1.Succeeded)
             {
-                foreach (var error in result.Errors)
-                {
-                    ModelState.TryAddModelError(error.Code, error.Description);
-                }
+                foreach (var error in result.Item1.Errors) ModelState.TryAddModelError(error.Code, error.Description);
                 return BadRequest(ModelState);
             }
-            await _userManager.AddToRoleAsync(user, userForRegistration.Role.ToString());
+            await _userManager.AddToRoleAsync(result.Item2, userForRegistration.Role.ToString());
 
-            return Ok(user);
+            return Ok(result.Item2);
         }
 
 
@@ -95,26 +94,21 @@ namespace LibraryApi.Controllers
             }
             return Ok(user);
         }
-
-
         
         [HttpPatch("update/{id}")]
         public async Task<IActionResult> UpdateUser(string id, [FromBody] JsonPatchDocument<UserForUpdateDTO> userForUpdate)
         {
-            if (userForUpdate == null)
-            {
-                return BadRequest("Object is null");
-            }
+            if (userForUpdate == null) return BadRequest("Object is null");
+            var user = await _userService.PatchUser(id, userForUpdate);
+            if (user == null) return BadRequest("User not found!");
+            /**
             var user = await _userManager.FindByIdAsync(id);
-            if (user == null)
-            {
-                return BadRequest("User not found!");
-            }
+            if (user == null) return BadRequest("User not found!");
             
             var userToUpdate = _mapper.Map<UserForUpdateDTO>(user);
             userForUpdate.ApplyTo(userToUpdate);
             _mapper.Map(userToUpdate, user);
-            
+            **/
             await _userManager.UpdateAsync(user);
             
             return Ok(user);
